93ce9ed17934a8f0f05a6aa7e059095ca91c9898
Mikl Barton <miklbarton@gmail.com>
2021-03-24 15:59:35 -0600

    ec2 deploy

--- QAService/app.js
+++ QAService/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mountRoutes = require('./routes');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const app = express();
 
