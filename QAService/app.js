b053ae5dd569df0b5a6a87d4c46e1301911d0d68
Mikl Barton <miklbarton@gmail.com>
2021-03-24 17:17:44 -0600

    8080

--- QAService/app.js
+++ QAService/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mountRoutes = require('./routes');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
