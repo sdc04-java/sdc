5b3e2077d71d19d2ee343826c9821083051eda5a
Mikl Barton <miklbarton@gmail.com>
2021-03-24 19:00:35 -0600

    update

--- QAService/routes/questions.js
+++ QAService/routes/questions.js
@@ -8,23 +8,24 @@ module.exports = router;
 
 router.route('/')
   .get(async (req, res) => {
-    try {
-      const { product_id, page, count } = req.query;
-      const first = page * count - count;
-      const last = page * count;
-      // const results = await db.listAll(product_id);
-      const results = await db.listQuestions(product_id);
-      console.log(product_id);
-      const pageCount = results.slice(first, last);
-      const hasAnswers = await helper.questionAnswers(pageCount);
-      const allData = await helper.questionPhotos(hasAnswers);
-      const final = await helper.questionShaper(allData, product_id);
+    res.status(200).send('hey');
+    // try {
+    //   const { product_id, page, count } = req.query;
+    //   const first = page * count - count;
+    //   const last = page * count;
+    //   // const results = await db.listAll(product_id);
+    //   const results = await db.listQuestions(product_id);
+    //   console.log(product_id);
+    //   const pageCount = results.slice(first, last);
+    //   const hasAnswers = await helper.questionAnswers(pageCount);
+    //   const allData = await helper.questionPhotos(hasAnswers);
+    //   const final = await helper.questionShaper(allData, product_id);
 
-      res.status(200).send(final);
-    } catch (error) {
-      console.error(error);
-      res.status(404).send(error);
-    }
+    //   res.status(200).send(final);
+    // } catch (error) {
+    //   console.error(error);
+    //   res.status(404).send(error);
+    // }
   })
   .post(async (req, res) => {
     try {
