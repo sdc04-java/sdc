73e27b35b4102c49ca3a2f5bcdbe1154a454fb09
Mikl Barton <miklbarton@gmail.com>
2021-03-25 09:29:01 -0600

    update

--- QAService/db/db-scripts/2-import.sql
+++ QAService/db/db-scripts/2-import.sql
@@ -1,5 +1,5 @@
 SET search_path = sdc;
-COPY sdc.questions FROM '/csv/questions.csv' WITH (FORMAT csv, header);
-COPY sdc.answers FROM '/csv/answers.csv' WITH (FORMAT csv, header);
-COPY sdc.photos FROM '/csv/answers_photos.csv' WITH (FORMAT csv, header);
+COPY sdc.questions FROM '/home/ubuntu/sdc/QAService/db/db-scripts/csv/questions.csv' WITH (FORMAT csv, header);
+COPY sdc.answers FROM '/home/ubuntu/sdc/QAService/db/db-scripts/csv/answers.csv' WITH (FORMAT csv, header);
+COPY sdc.photos FROM '/home/ubuntu/sdc/QAService/db/db-scripts/csv/answers_photos.csv' WITH (FORMAT csv, header);
 
