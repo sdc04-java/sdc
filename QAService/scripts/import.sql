7b5ac204d8cfc55519491aab47ead2f512e5d63f
Mikl Barton <miklbarton@gmail.com>
2021-03-25 09:22:34 -0600

    update

--- QAService/scripts/import.sql
+++ QAService/scripts/import.sql
@@ -1,5 +1,5 @@
 SET search_path = sdc;
-COPY sdc.questions FROM '/questions.csv' WITH (FORMAT csv, header);
-COPY sdc.answers FROM '/answers.csv' WITH (FORMAT csv, header);
-COPY sdc.photos FROM '/answers_photos.csv' WITH (FORMAT csv, header);
+COPY sdc.questions FROM '/csv/questions.csv' WITH (FORMAT csv, header);
+COPY sdc.answers FROM '/csv/answers.csv' WITH (FORMAT csv, header);
+COPY sdc.photos FROM '/csv/answers_photos.csv' WITH (FORMAT csv, header);
 
