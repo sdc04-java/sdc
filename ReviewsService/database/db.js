ddd0b3303361e7dd7c4d693c659358dd272648a3
Lerroy Mwaghore <lmwaghore@gmail.com>
2021-03-25 07:07:14 -0500

    added sort func

--- ReviewsService/database/db.js
+++ ReviewsService/database/db.js
@@ -8,10 +8,22 @@ const pool = new Pool ({
   port: 5432
 });
 
-const getAllReviews = (obj, cb) => {
-  pool.query(`SELECT reviewslist.review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness, review_photos.id, url FROM reviewslist LEFT JOIN review_photos ON reviewslist.review_id = review_photos.review_id WHERE product_id = ${obj.product_id}`)
+const getAllReviews = ({page, count, sort, product_id}, cb) => {
+  let dbSorter;
+  switch(sort) {
+    case 'newest':
+      dbSorter = 'date';
+      break;
+    case 'helpful':
+    case 'relevant':
+      dbSorter = 'helpfulness';
+      break;
+    default:
+      dbSorter = 'helpfulness';
+  }
+  pool.query(`SELECT reviewslist.review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness, review_photos.id, url FROM reviewslist LEFT JOIN review_photos ON reviewslist.review_id = review_photos.review_id WHERE product_id = ${product_id} ORDER BY ${dbSorter} DESC`)
   .then((results) => {
-    cb(results.rows.slice(0, obj.count));
+    cb(results.rows.slice(0, count));
   })
   .catch((err) => {
     cb(err);
